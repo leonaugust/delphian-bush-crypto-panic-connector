3eb4b58576010792d61c5ce492df4327b689125a
Leon Galushko <leon-galushko@mail.ru>
2022-06-04 18:45:57 +0300

    extracted schema variables to global constants

---
+++ src/main/java/com/delphian/bush/schema/CurrenciesSchema.java
@@ -0,0 +1,28 @@
+package com.delphian.bush.schema;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+
+public class CurrenciesSchema {
+
+
+    public static final String CURRENCIES_SCHEMA_NAME = "currencies";
+
+    public static final Integer FIRST_VERSION = 1;
+
+    public static final String CODE_FIELD = "code";
+    public static final String TITLE_FIELD = "title";
+    public static final String SLUG_FIELD = "slug";
+    public static final String URL_FIELD = "url";
+
+    public static final Schema CURRENCIES_SCHEMA = SchemaBuilder.struct()
+            .name(CURRENCIES_SCHEMA_NAME)
+            .field(CODE_FIELD, Schema.OPTIONAL_STRING_SCHEMA)
+            .field(TITLE_FIELD, Schema.OPTIONAL_STRING_SCHEMA)
+            .field(SLUG_FIELD, Schema.OPTIONAL_STRING_SCHEMA)
+            .field(URL_FIELD, Schema.OPTIONAL_STRING_SCHEMA)
+            .version(FIRST_VERSION)
+            .optional()
+            .build();
+
+}
