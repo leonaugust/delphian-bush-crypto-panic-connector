93d4a4e63467f2f6b3d1724748f0f52bffd885f2
Leon <leon-galushko@mail.ru>
2022-12-07 20:46:27 +0300

    simplified news methods

--- src/main/java/com/delphian/bush/service/CryptoPanicService.java
+++ src/main/java/com/delphian/bush/service/CryptoPanicService.java
@@ -1,11 +1,13 @@
 package com.delphian.bush.service;
 
+import com.delphian.bush.dto.CryptoNews;
 import com.delphian.bush.dto.CryptoNewsResponse;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CryptoPanicService {
 
-    CryptoNewsResponse getCryptoNewsByProfile(boolean fetchAllPreviousNews, Optional<Long> sourceOffset);
+    List<CryptoNews> getCryptoNewsByProfile(boolean fetchAllPreviousNews, Optional<Long> sourceOffset);
 
 }
