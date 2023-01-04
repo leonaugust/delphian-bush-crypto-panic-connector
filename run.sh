4dade8b214de4ea28482d69a76dcd1433bbdb00d
Leon <leon-galushko@mail.ru>
2023-01-04 14:24:56 +0300

    fixed version in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export CLASSPATH="$(find target/ -type f -name '*.jar'| grep '\-package' | tr '\n' ':')"
-    docker build . -t delphianbush/crypto-panic-connector:1.0
+    docker build . -t delphianbush/crypto-panic-connector:0.0.1
     docker run --name news-connector --net=host --rm -t \
            -v $(pwd)/offsets:/crypto-panic-connector/offsets \
-           delphianbush/crypto-panic-connector:1.0
+           delphianbush/crypto-panic-connector:0.0.1
